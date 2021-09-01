7d39d574710ab81cf40bdd58a46a49475a65d013
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-01 21:28:36 +0900

    formatting

--- src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
+++ src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+     SpringApplication.run(DemoApplication.class, args);
+  }
 
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
