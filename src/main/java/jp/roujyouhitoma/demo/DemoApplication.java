96f209c952b5b065679fb021a0add48679232e48
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-02 20:20:42 +0900

    Add /hello2

--- src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
+++ src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
@@ -14,6 +14,11 @@ public class DemoApplication {
      SpringApplication.run(DemoApplication.class, args);
   }
 
+  @GetMapping("/hello2")
+  public String hello() {
+     return String.format("Hello World");
+  }
+
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
      return String.format("Hello %s!", name);
