258eb20782f2ba8ed2fa06fec52166173077510d
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-03 21:19:29 +0900

    Update src/test/java/jp/roujyouhitoma/demo/DemoApplicationTests.java

--- src/test/java/jp/roujyouhitoma/demo/DemoApplicationTests.java
+++ src/test/java/jp/roujyouhitoma/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package jp.roujyouhitoma.demo;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 }
