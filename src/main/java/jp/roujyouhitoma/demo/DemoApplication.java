699fc170c2c207461cb6dd73e60cccba9df25af8
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-06 18:30:14 +0900

    Apply formatting

--- src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
+++ src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
@@ -10,17 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-  public static void main(String[] args) {
-     SpringApplication.run(DemoApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-  @GetMapping("/hello2")
-  public String hello() {
-     return String.format("Hello World!");
-  }
+	@GetMapping("/hello2")
+	public String hello() {
+		return String.format("Hello World!");
+	}
 
-  @GetMapping("/hello")
-  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-     return String.format("Hello %s!", name);
-  }
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
 }
