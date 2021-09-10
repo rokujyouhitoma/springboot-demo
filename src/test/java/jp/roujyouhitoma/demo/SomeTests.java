acd3159546223dc79c08b3556e52edf87433cdd9
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-10 21:41:10 +0900

    Add testcase

--- src/test/java/jp/roujyouhitoma/demo/SomeTests.java
+++ src/test/java/jp/roujyouhitoma/demo/SomeTests.java
@@ -1,12 +1,31 @@
 import jp.roujyouhitoma.demo.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.DisplayName;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class SomeTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void getgreeting() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().string(containsString("Hello, World!")));
+	}
 
 }
