dd0dc207edfc0b3e52b4392453f6f0bcca9fcf35
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-23 09:45:49 +0900

    Update  src/main/java/jp/roujyouhitoma/demo/DemoApplication.java

--- src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
+++ src/main/java/jp/roujyouhitoma/demo/DemoApplication.java
@@ -9,7 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 class User {
 
+	String id;
+
 	String name;
+
+	User(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return this.id;
+	}
 
 	public String getName() {
 		return this.name;
